67ba4898aded2ca9a096ccb363d6dcc43ab69a2d
karthickramalingam <KarthickRamalingam@github.com>
2016-07-20 13:06:33 +0530

    Additions to the existing dc-on-docker with 16 nodes 4 Fabs, 8 Spines, 4 Leafs and 2 servers

--- utils/docker-network.sh
+++ utils/docker-network.sh
@@ -11,66 +11,122 @@ net_prefix="znet"
 # create fabric networks
 create_fabric_networks()
 {
-	for i in {0..2}; do
-		docker network inspect ${net_prefix}${i} > /dev/null
-		if [ $? != 0 ]; then
-			docker network create --internal \
-				--subnet=172.16.${i}.0/24 \
-				--gateway=172.16.${i}.254 ${net_prefix}${i}
-		fi
+	for j in {1..4}; do
+		for i in {1..8}; do
+			docker network inspect ${net_prefix}1${j}${i} > /dev/null
+			if [ $? != 0 ]; then
+				docker network create  \
+					--subnet=10.10.${j}${i}.0/24 \
+					--gateway=10.10.${j}${i}.254 ${net_prefix}1${j}${i}
+			fi
+		done
 	done
+
+        for i in {1..4}; do
+                docker network inspect ${net_prefix}41${i} > /dev/null
+                if [ $? != 0 ]; then
+                        docker network create  \
+                                --subnet=10.40.1${i}.0/24 \
+                                --gateway=10.40.1${i}.254 ${net_prefix}41${i}
+                fi
+        done
+
 }
 
 # create spine networks
 create_spine_networks()
 {
-	for j in {1..2}; do
-		for i in {1..2}; do
-			docker network inspect ${net_prefix}${j}${i} > /dev/null
-			if [ $? != 0 ]; then
-				docker network create --internal \
-					--subnet=172.16.${j}${i}.0/24 \
-					--gateway=172.16.${j}${i}.254 \
-					${net_prefix}${j}${i}
-			fi
-		done
+	for i in {1..8}; do
+		docker network inspect ${net_prefix}21${i} > /dev/null
+		if [ $? != 0 ]; then
+			docker network create  \
+				--subnet=10.20.1${i}.0/24 \
+				--gateway=10.20.1${i}.254 \
+				${net_prefix}21${i}
+		fi
 	done
+        for i in {1..8}; do
+                docker network inspect ${net_prefix}22${i} > /dev/null
+                if [ $? != 0 ]; then 
+                        docker network create \
+                                --subnet=10.20.2${i}.0/24 \
+                                --gateway=10.20.2${i}.254 \
+                                ${net_prefix}22${i}
+                fi
+        done
 }
 
 # create leaf networks
 create_leaf_networks()
 {
-	for i in 30 31 40 41 50 51 60 61; do
-		docker network inspect ${net_prefix}${i} > /dev/null
-		if [ $? != 0 ]; then
-			docker network create --internal \
-				--subnet=172.16.${i}.0/24 \
-				--gateway=172.16.${i}.254 \
-				${net_prefix}${i}
-		fi
-	done
+        for i in {1..4}; do
+                docker network inspect ${net_prefix}51${i} > /dev/null
+                if [ $? != 0 ]; then
+                        docker network create  \
+                                --subnet=10.50.1${i}.0/24 \
+                                --gateway=10.50.1${i}.254 ${net_prefix}51${i}
+                fi
+        done
+
 }
 
 # connect fabric switch
 connect_fabric_switches()
 {
-	docker inspect fab1 > /dev/null
-	if [ $? = 0 ]; then
-		docker network connect ${net_prefix}0 fab1
-		docker network connect ${net_prefix}1 fab1
-		docker network connect ${net_prefix}2 fab1
-	fi
+
+        for j in {1..4}; do
+                docker inspect fab${j} > /dev/null
+                if [ $? = 0 ]; then
+                        for i in {1..8}; do
+                                docker network connect ${net_prefix}1${j}${i} fab${j}
+				docker inspect spine${i} > /dev/null
+                                if [ $? = 0 ]; then
+					docker network connect ${net_prefix}1${j}${i} spine${i}
+				fi
+                        done
+                fi
+        done
 }
 
 # connect servers to fabric networks
 connect_servers_to_fabric_networks()
 {
-	for i in {1..2}; do
-		docker inspect server${i} > /dev/null
-		if [ $? = 0 ]; then
-			docker network connect ${net_prefix}${i} server${i}
-		fi
+	docker inspect server1 > /dev/null
+	if [ $? = 0 ]; then
+                docker network connect ${net_prefix}411 fab1
+                docker network connect ${net_prefix}412 fab2
+                docker network connect ${net_prefix}413 fab3
+                docker network connect ${net_prefix}414 fab4
+		docker network connect ${net_prefix}411 server1
+                docker network connect ${net_prefix}412 server1
+                docker network connect ${net_prefix}413 server1
+                docker network connect ${net_prefix}414 server1
+        fi
+}
+
+# connect leaf switches
+connect_leaf_switches()
+{
+	for j in {1..4}; do
+       		for i in {1..2}; do
+	                docker inspect leaf${i} > /dev/null
+        	        if [ $? = 0 ]; then
+				docker network connect ${net_prefix}2${i}${j} spine${j}
+				docker network connect ${net_prefix}2${i}${j} leaf${i}
+			fi
+		done
 	done
+        for j in {5..8}; do
+                for i in {1..2}; do
+			index=`expr ${i} + 2`
+                        docker inspect leaf${index} > /dev/null
+                        if [ $? = 0 ]; then
+                                docker network connect ${net_prefix}2${i}${j} spine${j}
+                                docker network connect ${net_prefix}2${i}${j} leaf${index}
+                        fi
+                done
+        done
+
 }
 
 # connect spine switches
@@ -79,47 +135,14 @@ connect_spine_switches()
 	for i in {1..2}; do
 		docker inspect spine${i} > /dev/null
 		if [ $? = 0 ]; then
-			docker network connect ${net_prefix}${i} spine${i}
-			docker network connect ${net_prefix}${i}1 spine${i}
-			docker network connect ${net_prefix}${i}2 spine${i}
-		fi
-	done
-}
-
-# connect servers to spine networks
-connect_servers_to_spine_networks()
-{
-	for i in 11 12 21 22; do
-		docker inspect server${i} > /dev/null
-		if [ $? = 0 ]; then
-			docker network connect ${net_prefix}${i} server${i}
-		fi
-	done
-}
-
-# connect leaf switches
-connect_leaf_switches()
-{
-	for i in {1..2}; do
-		docker inspect leaf${i} > /dev/null
-		if [ $? = 0 ]; then
-			docker network connect ${net_prefix}1${i} \
+			docker network connect ${net_prefix}$(expr ${i} + 105) \
 				leaf${i}
-			docker network connect ${net_prefix}$(expr ${i} + 2)0 \
+			docker network connect ${net_prefix}$(expr ${i} + 105 + 2) \
 				leaf${i}
-			docker network connect ${net_prefix}$(expr ${i} + 2)1 \
-				leaf${i}
-		fi
-	done
-	for i in {3..4}; do
-		docker inspect leaf${i} > /dev/null
-		if [ $? = 0 ]; then
-			docker network connect ${net_prefix}2$(expr ${i} - 2) \
-				leaf${i}
-			docker network connect ${net_prefix}$(expr ${i} + 2)0 \
-				leaf${i}
-			docker network connect ${net_prefix}$(expr ${i} + 2)1 \
-				leaf${i}
+                        docker network connect ${net_prefix}$(expr ${i} + 205) \
+                                leaf${i}
+                        docker network connect ${net_prefix}$(expr ${i} + 205 + 2) \
+                                leaf${i} 
 		fi
 	done
 }
@@ -127,12 +150,17 @@ connect_leaf_switches()
 # connect servers to leaf networks
 connect_servers_to_leaf_networks()
 {
-	for i in 30 31 40 41 50 51 60 61; do
-		docker inspect server${i} > /dev/null
-		if [ $? = 0 ]; then
-			docker network connect ${net_prefix}${i} server${i}
-		fi
-	done
+	docker inspect server2 > /dev/null
+        if [ $? = 0 ]; then
+                docker network connect ${net_prefix}511 leaf1
+                docker network connect ${net_prefix}512 leaf2
+                docker network connect ${net_prefix}513 leaf3
+                docker network connect ${net_prefix}514 leaf4
+                docker network connect ${net_prefix}511 server2
+                docker network connect ${net_prefix}512 server2
+                docker network connect ${net_prefix}513 server2
+                docker network connect ${net_prefix}514 server2
+        fi
 }
 
 # main
@@ -140,6 +168,7 @@ create_fabric_networks
 create_spine_networks
 create_leaf_networks
 connect_fabric_switches
-connect_spine_switches
+#connect_spine_switches
 connect_leaf_switches
 connect_servers_to_fabric_networks
+connect_servers_to_leaf_networks
